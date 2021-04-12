6d9a4ba806c8ad9781f806250f232004ed182682
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-04-11 20:56:24 -0400

    added axios to api modules, added more content inside the API folder to amke sure we get the random images and puppie breeds

---
+++ src/utils/API.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    getRandomDog: function() {
+        return axios.get('https://dog.ceo/api/breeds/image/random');
+    },
+    getDogsOfBreed: function(breed) {
+        return axios.get("https://dog.ceo/api/breed/" + breed + "/images")
+    },
+    getBaseBreedsList: function() {
+        return axios.get("https://dog.ceo/api/breeds/list");
+    }
+}
